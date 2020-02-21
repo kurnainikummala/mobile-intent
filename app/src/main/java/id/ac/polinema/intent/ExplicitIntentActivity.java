21f70df4b0917b7eb3f863e45f3614160454d9a3
kurnainikummala <kurnainikummala@gmail.com>
2020-02-21 11:30:39 +0700

    intent

--- app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -1,15 +1,24 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
-
+    private EditText nameInput;
+    private TextView outputText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        nameInput = (EditText)findViewById(R.id.input_name);
+        outputText = (TextView)findViewById(R.id.text_output);
+    }
+    public void TampilNama(View view){
+        outputText.setText("Hello "+nameInput.getText()+"Congratulation");
     }
 
 }
---
+++ app/src/main/java/model/User.java
@@ -0,0 +1,48 @@
+package model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
+    private String username;
+    private String name;
+    private int age;
+
+    public User(String username, String name, int age){
+        this.username=username;
+        this.name=name;
+        this.age=age;
+    }
+    protected User (Parcel in){
+        username=in.readString();
+        name=in.readString();
+        age=in.readInt();
+    }
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+        @Override
+        public User[] newArray(int size){
+            return new User[size];
+        }
+
+    };
+    public void setUsername(String username){this.username = username; }
+    public void setName(String name) {this.name = name; }
+    public void setAge(int age) {this.age=age; }
+    public String getUsername() {return username; }
+    public String getName () {return name; }
+    public int getAge() {return age; }
+
+    @Override
+    public int describeContents() { return 0; }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags){
+        dest.writeString(username);
+        dest.writeString(name);
+        dest.writeInt(age);
+    }
+}
